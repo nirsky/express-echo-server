1652cb02e9da72c1a6b6e1b8489def0abb1c3f3c
nirsky <nir@aspecto.io>
2020-08-23 13:36:56 +0300

    Added full url

--- src/echo-route.ts
+++ src/echo-route.ts
@@ -1,14 +1,15 @@
-import {Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 import * as prettyjson from 'prettyjson';
 
 const router = Router();
-const RESET_TOKEN = "\x1b[0m";
-const UNDERSCORE_TOKEN = "\x1b[4m";
+const RESET_TOKEN = '\x1b[0m';
+const UNDERSCORE_TOKEN = '\x1b[4m';
 
 const logRequest = (req: Request) => {
     console.log(`\n\n🆕 NEW ${UNDERSCORE_TOKEN}${req.method.toLocaleUpperCase()}${RESET_TOKEN} REQUEST 🆕
     
 • Path: ${req.path}
+• Full Url: ${req.url}
 
 • Headers: 
 ${prettyjson.render(req.headers)}
@@ -17,14 +18,16 @@ ${prettyjson.render(req.headers)}
 ${prettyjson.render(req.query)}
 
 • Payload:
-${prettyjson.render(req.body)}`)
+${prettyjson.render(req.body)}`);
 };
 
-['get', 'post', 'put', 'patch', 'delete', 'copy', 'head', 'options', 'purge', 'lock', 'unlock', 'propfind'].forEach(method => {
-    router[method]('/*', (req: Request, res: Response) => {
-        logRequest(req);
-        res.status(200).send(true)
-    });
-})
+['get', 'post', 'put', 'patch', 'delete', 'copy', 'head', 'options', 'purge', 'lock', 'unlock', 'propfind'].forEach(
+    (method) => {
+        router[method]('/*', (req: Request, res: Response) => {
+            logRequest(req);
+            res.status(200).send(true);
+        });
+    }
+);
 
 export default router;
